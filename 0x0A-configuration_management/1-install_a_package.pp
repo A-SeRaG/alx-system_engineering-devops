5923a4b3d77821ef57a08cf65856b7e50f51b5c2
SeRaG <aserag0101@gmail.com>
2024-06-28 12:56:37 -0700

    programming

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,6 @@
 # Ensure python3-pip is installed
 
-package { 'python3-pip':
-  ensure => installed,
-}
-
-
-# Ensure Flask version 2.1.0 is installed using pip3
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
