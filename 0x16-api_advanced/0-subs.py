02194f6f9e844481c2c0d0b5a88ac50b1d958615
SeRaG <aserag0101@gmail.com>
2024-08-13 07:36:52 -0700

    programming

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-"""Script that returns the number of subscribers"""
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
 
-import request
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers"""
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
