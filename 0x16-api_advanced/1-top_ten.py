5fb9b811f5cd4730e25c8ddc5d471fe721cf9d90
Ahmed Serag Hamed <138832368+A-SeRaG@users.noreply.github.com>
2024-08-14 03:18:25 +0300

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,15 +10,28 @@ def top_ten(subreddit):
     '''
         returns the top ten posts for a given subreddit
     '''
-    user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
-                       .format(subreddit), headers=user).json()
-    try:
-        for post in url.get('data').get('children'):
-            print(post.get('data').get('title'))
-    except Exception:
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "Mozilla/5.0"}
+
+    # Set the parameters for the query to limit to 10 posts
+    params = {"limit": 10}
+
+    # Make the request to the Reddit API
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+
+    # Check if the request was successful
+    if response.status_code != 200:
         print(None)
+        return
 
+    # Parse the JSON response
+    data = response.json()
 
-if __name__ == "__main__":
-    top_ten(argv[1])
+    # Check if there are any hot posts
+    if "data" not in data or "children" not in data["data"]:
+        print(None)
+        return
+
+    # Print the titles of the first 10 hot posts
+    for post in data["data"]["children"]:
+        print(post["data"]["title"])
